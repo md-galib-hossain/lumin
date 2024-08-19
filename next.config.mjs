1502550292c5a0da7cf7fdd4376c27f9781ff002
Md Galib Hossain <mdgalib23@gmail.com>
2024-08-20 01:02:32 +0600

    feat: lucia auth create

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    //client side caching for 30 seconds
+    experimental : {
+        staleTimes : {
+            dynamic:30
+        }
+    }
+};
 
 export default nextConfig;
