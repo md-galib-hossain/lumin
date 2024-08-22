6b93fcdbbca9f6231d490c0ab405817bb2d21eba
Md Galib Hossain <mdgalib23@gmail.com>
2024-08-22 20:34:31 +0600

    feat : who to follow & trending topics section at homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,7 @@ export default function RootLayout({
 
         {children}
       </NextThemesProvider>
+      <Toaster/>
       </body>
     </html>
   );
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,27 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import {formatDate, formatDistanceToNowStrict} from "date-fns"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const formatRelativeDate=(from:Date)=>{
+const currentDate = new Date()
+if(currentDate.getTime()-from.getTime() < 24 * 60 * 60 * 1000){
+  return formatDistanceToNowStrict(from,{addSuffix:true})
+}else{
+  if(currentDate.getFullYear() === from.getFullYear()){
+    return formatDate(from,"MMM d")
+  }else{
+    return formatDate(from,"MMM d, yyyy")
+  }
+}
+}
+
+//number of posts with tags
+export const  formateNumber=(n:number):string=>{
+  return Intl.NumberFormat("en-US", {
+    notation : "compact",
+    maximumFractionDigits : 1
+  }).format(n)
 }
