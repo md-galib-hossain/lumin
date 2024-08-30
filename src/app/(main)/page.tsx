3590fcae4a5de97e73522509aed44e822a5d0eab
Md Galib Hossain <mdgalib23@gmail.com>
2024-08-30 11:59:50 +0600

    feat: following users feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,16 +3,28 @@ import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import ForYouFeed from "./ForYouFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import FollowingFeed from "./FollowingFeed";
 
 export default function Home() {
-
   return (
-    <main className="w-full min-w-0 flex gap-5">
-      <div className="w-full min-w-0 space-y-5">
-        <PostEditor />
-      <ForYouFeed/>
-      </div>
-      <TrendsSidebar/>
-    </main>
+    <main className="flex w-full min-w-0 gap-5">
+    <div className="w-full min-w-0 space-y-5">
+      <PostEditor />
+      <Tabs defaultValue="for-you">
+        <TabsList>
+          <TabsTrigger value="for-you">For you</TabsTrigger>
+          <TabsTrigger value="following">Following</TabsTrigger>
+        </TabsList>
+        <TabsContent value="for-you">
+          <ForYouFeed />
+        </TabsContent>
+        <TabsContent value="following">
+          <FollowingFeed />
+        </TabsContent>
+      </Tabs>
+    </div>
+    <TrendsSidebar />
+  </main>
   );
 }
