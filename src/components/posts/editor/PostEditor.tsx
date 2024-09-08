1dc0b3c1288d32b060c300f59db1daef2fcb4d6a
Md Galib Hossain <mdgalib23@gmail.com>
2024-09-08 23:05:11 +0600

    Feat: Drag & drop  copy paste attachment on input for upload

--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -10,10 +10,12 @@ import "./styles.css";
 import { useSubmitPostMutation } from "./mutation";
 import LoadingButton from "@/components/LoadingButton";
 import useMediaUpload, { Attachment } from "./useMediaUpload";
-import { useRef } from "react";
+import { ClipboardEvent, useRef } from "react";
 import { ImageIcon, Loader2, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { useDropzone } from "@uploadthing/react";
+import { HtmlContext } from "next/dist/server/future/route-modules/pages/vendored/contexts/entrypoints";
 const PostEditor = () => {
   const { user } = useSession();
   const mutation = useSubmitPostMutation();
@@ -25,6 +27,12 @@ const PostEditor = () => {
     removeAttachment,
     reset: resetMediaUpload,
   } = useMediaUpload();
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: startUpload,
+  });
+  const { onClick, ...rootProps } = getRootProps();
+
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
@@ -57,14 +65,28 @@ const PostEditor = () => {
     );
   };
 
+  function onPaste(e: ClipboardEvent<HTMLInputElement>) {
+    const files = Array.from(e.clipboardData.items)
+      .filter((item) => item.kind === "file")
+      .map((item) => item.getAsFile()) as File[];
+    startUpload(files);
+  }
+
   return (
     <div className="flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm">
       <div className="flex gap-5">
         <UserAvatar avatarUrl={user.avatarUrl} className="hidden sm:inline" />
-        <EditorContent
-          editor={editor}
-          className="max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3"
-        />
+        <div {...rootProps} className="w-full">
+          <EditorContent
+            editor={editor}
+            className={cn(
+              "max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-background px-5 py-3",
+              isDragActive && "outline-dashed",
+            )}
+            onPaste={onPaste}
+          />
+          <input {...getInputProps()} />
+        </div>
       </div>
       {!!attachments.length && (
         <AttachmentPreviews
