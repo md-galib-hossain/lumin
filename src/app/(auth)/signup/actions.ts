0f70e5e6c72e7e930f27dd5a0f16364f1730c477
Md Galib Hossain <mdgalib23@gmail.com>
2024-09-12 19:47:25 +0600

    Feat: Message user using stream

--- src/app/(auth)/signup/actions.ts
+++ src/app/(auth)/signup/actions.ts
@@ -2,6 +2,7 @@
 
 import { lucia } from "@/auth";
 import prisma from "@/lib/prisma";
+import streamServerClient from "@/lib/stream";
 import { signUpSchema, SignUpValues } from "@/lib/validation";
 import { hash } from "@node-rs/argon2";
 import { generateIdFromEntropySize } from "lucia";
@@ -13,13 +14,16 @@ export async function signUp(
 ): Promise<{ error: string }> {
   try {
     const { username, email, password } = signUpSchema.parse(credentials);
+
     const passwordHash = await hash(password, {
       memoryCost: 19456,
       timeCost: 2,
       outputLen: 32,
       parallelism: 1,
     });
+
     const userId = generateIdFromEntropySize(10);
+
     const existingUsername = await prisma.user.findFirst({
       where: {
         username: {
@@ -28,11 +32,13 @@ export async function signUp(
         },
       },
     });
+
     if (existingUsername) {
       return {
         error: "Username already taken",
       };
     }
+
     const existingEmail = await prisma.user.findFirst({
       where: {
         email: {
@@ -41,20 +47,30 @@ export async function signUp(
         },
       },
     });
+
     if (existingEmail) {
       return {
         error: "Email already taken",
       };
     }
-    await prisma.user.create({
-      data: {
+
+    await prisma.$transaction(async (tx) => {
+      await tx.user.create({
+        data: {
+          id: userId,
+          username,
+          displayName: username,
+          email,
+          passwordHash,
+        },
+      });
+      await streamServerClient.upsertUser({
         id: userId,
         username,
-        displayName: username,
-        email,
-        passwordHash,
-      },
+        name: username,
+      });
     });
+
     const session = await lucia.createSession(userId, {});
     const sessionCookie = lucia.createSessionCookie(session.id);
     cookies().set(
@@ -62,12 +78,13 @@ export async function signUp(
       sessionCookie.value,
       sessionCookie.attributes,
     );
+
     return redirect("/");
-  } catch (err) {
-    if(isRedirectError(err)) throw err
-    console.log(err);
+  } catch (error) {
+    if (isRedirectError(error)) throw error;
+    console.error(error);
     return {
-      error: "Something went wrong. Please try again",
+      error: "Something went wrong. Please try again.",
     };
   }
 }
