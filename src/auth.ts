0e6cdbefe3dc50aa83709d3731b1bbb017728551
Md Galib Hossain <mdgalib23@gmail.com>
2024-09-22 12:27:33 +0600

    Feat: Google Oauth integrate

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import prisma from "./lib/prisma";
 import { Lucia, Session, User } from "lucia";
 import { cache } from "react";
 import { cookies } from "next/headers";
+import {Google} from "arctic"
 const adapter = new PrismaAdapter(prisma.session, prisma.user);
 
 export const lucia = new Lucia(adapter, {
@@ -36,6 +37,13 @@ interface DatabaseUserAttributes {
   avatarUrl: string | null;
   googleId: string | null;
 }
+
+export const google = new Google(
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
+  `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback/google`
+)
+
 //cached for only same request for multiple components
 export const validateRequest = cache(
   async (): Promise<
